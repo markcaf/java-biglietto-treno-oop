815bb564a98670ebc10dcc0dd127333c1d0754ee
Marco Cafasso <marcocafassog@gmail.com>
2022-11-21 14:49:00 +0100

    feat: add methods to validate userKm and userAge

--- Train Ticket/src/org/buyticket/italy/Ticket.java
+++ Train Ticket/src/org/buyticket/italy/Ticket.java
@@ -1,5 +1,43 @@
 package org.buyticket.italy;
 
 public class Ticket {
+	
+	private int userKm;
+	private int userAge;
+	
+	public Ticket (int userKm, int userAge) throws Exception {
+		isValidKm(userKm);
+		isValidAge(userAge);
+	}
+
+	public int getUserKm() {
+		return userKm;
+	}
+
+	public void setUserKm(int userKm) {
+		this.userKm = userKm;
+	}
+
+	public int getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	
+	public void isValidKm(int km) throws Exception{
+		if (km < 0) {
+			throw new Exception("Il numero di Km deve essere positivo");
+		} else {
+			this.userKm = km;
+		}
+	}
+	
+	public void isValidAge(int age) throws Exception{
+		if (age <= 0) {
+			throw new Exception("L'età deve essere superiore a 0");
+		}
+	}
 
 }
