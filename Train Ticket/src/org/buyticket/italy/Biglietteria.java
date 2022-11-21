5f38364b9924e0a2d24441650e4c2d5b1de24e5f
Marco Cafasso <marcocafassog@gmail.com>
2022-11-21 16:02:52 +0100

    feat: Milestone 2 complete
    - ask the user to enter the number of km and the age of the passenger
    - with that data created a new Ticket (manage any exceptions with try-catch)
    - print the calculated ticket price on screen

--- Train Ticket/src/org/buyticket/italy/Biglietteria.java
+++ Train Ticket/src/org/buyticket/italy/Biglietteria.java
@@ -1,9 +1,30 @@
 package org.buyticket.italy;
 
+import java.util.Scanner;
+
 public class Biglietteria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of km to travel: ");
+		int userKm = sc.nextInt();
+		
+		System.out.print("Enter the age of the passenger: ");
+		int userAge = sc.nextInt();
+		
+		sc.close();
+		System.out.println("--------------");
+		
+		Ticket userTicket = null;
+		try {
+			userTicket = new Ticket(userKm, userAge);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			System.out.println(userTicket);
+		}
 
 	}
 
